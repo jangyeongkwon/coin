c7bcd9c5f27b4dccae718e784843334a1fdaeba3
jangyeongkwon <90546830+jangyeongkwon@users.noreply.github.com>
2021-09-12 16:28:09 +0900

    Update bitcoinAutoTrade.py

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -44,8 +44,8 @@ while True:
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-ELF", 0.5)
-            current_price = get_current_price("KRW-elf")
+            target_price = get_target_price("KRW-ELF", -0.5)
+            current_price = get_current_price("KRW-ELF")
             if target_price < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
